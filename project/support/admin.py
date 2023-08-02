493aa186f2045ed0821906e89e30e8f2e74b052b
ppparkta <pppppark2@gamil.com>
2023-08-03 00:57:29 +0900

    fix: 블록체인 모델 어드민 추가 #2

--- project/support/admin.py
+++ project/support/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from support.models import Support, SupportForm, Bank
+from support.models import Support, SupportForm, Bank, Block
 
 # Register your models here.
 admin.site.register(Support)
 admin.site.register(SupportForm)
 admin.site.register(Bank)
+admin.site.register(Block)
